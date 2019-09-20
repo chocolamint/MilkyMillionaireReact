1b4450a057b749ef01da3418c4b174df49545a07
しゃるしぃ <info@angelbell.jp>
2019-09-20 17:17:55 +0900

    警告消す

--- milky-millionaire/src/models/Rule.ts
+++ milky-millionaire/src/models/Rule.ts
@@ -2,7 +2,7 @@ import { Card, NumberCard } from "./Card";
 
 function canDiscard(stackTop: readonly Card[] | undefined, cards: readonly Card[]) {
 
-    if (cards.length == 0) return false;
+    if (cards.length === 0) return false;
     if (cards.length >= 5) return false;
 
     let c: Card;
@@ -10,11 +10,11 @@ function canDiscard(stackTop: readonly Card[] | undefined, cards: readonly Card[
         c = cards[0];
     } else {
         c = cards.find(x => !x.joker)!;
-        if (cards.some(x => !x.joker && (c as NumberCard).rank != x.rank)) return false;
+        if (cards.some(x => !x.joker && (c as NumberCard).rank !== x.rank)) return false;
     }
     if (stackTop === undefined) return true;
     if (getHeight(c) <= getHeight(stackTop[0])) return false;
-    return cards.length == stackTop.length;
+    return cards.length === stackTop.length;
 }
 
 function getHeight(card: Card) {
