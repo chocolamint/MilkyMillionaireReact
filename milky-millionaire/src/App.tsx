af3e639cca6a15960d0b3b906a71707aab400c06
しゃるしぃ <info@angelbell.jp>
2019-09-21 16:25:31 +0900

    メタデータを分割

--- milky-millionaire/src/App.tsx
+++ milky-millionaire/src/App.tsx
@@ -5,6 +5,7 @@ import CardView from './components/CardView';
 import Player from './components/PlayerView';
 import { GameStatus, Random, shuffle, deal, TurnResult } from './models/Game';
 import { allCards, Card, cardToString } from './models/Card';
+import gameInfo from './GameInfo';
 
 interface DiscardedCards {
     by: number;
@@ -29,32 +30,6 @@ export default function App(props: { random: Random }) {
     const [discarding, setDiscarding] = useState(undefined as DiscardedCards | undefined);
     const [isTrickEnding, setIsTrickEnding] = useState(false);
 
-    const gameInfo = {
-        cpus: [{
-            name: "パクチー",
-            color: "#F189C8",
-            bgColor: "",
-            imageFileName: "vegetable_pakuchi_coriander.png"
-        }, {
-            name: "日本酒",
-            color: "#34BD67",
-            bgColor: "",
-            imageFileName: "masu_nihonsyu.png"
-        }, {
-            name: "餃子",
-            color: "#26C4F0",
-            bgColor: "",
-            imageFileName: "food_gyouza_mise.png"
-        }, {
-            name: "かまぼこ",
-            color: "#C97842",
-            bgColor: "",
-            imageFileName: "kamaboko_red.png"
-        }],
-        player: {
-            rank: "平民" as const,
-        },
-    };
     const message = "";
 
     return (
